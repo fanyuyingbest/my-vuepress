4fae0c2d0d5e15482bc5e18df0e1cda8320fc5a9
范玉英 <747249363@qq.com>
2020-06-16 16:29:21 +0800

    add data5

--- docs/senior-js/algorithm/sidebar.js
+++ docs/senior-js/algorithm/sidebar.js
@@ -23,6 +23,7 @@ module.exports = [
         '/senior-js/algorithm/data-structure/data-2',
         '/senior-js/algorithm/data-structure/data-3',
         '/senior-js/algorithm/data-structure/data-4',
+        '/senior-js/algorithm/data-structure/data-5',
     ]
 },
 ]
